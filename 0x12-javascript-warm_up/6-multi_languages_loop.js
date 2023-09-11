e72c08caaf17bd441907223357ee1954ecc6fc14
mohamed hossam <Baizmohamedhossam@gmail.com>
2023-09-11 05:05:03 -0700

    all tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+
+let i;
 const lines = [
   "C is fun",
   "Python is cool",
   "JavaScript is amazing"
 ];
-
-for (const line of lines) {
-  console.log(line);
+for (i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
 }
